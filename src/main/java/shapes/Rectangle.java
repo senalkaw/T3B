0692477644e04e2f193cbce6ff0cf3675ebeb8c2
senalkaw <senalkawickramaratne@hotmail.com>
2019-11-06 12:55:46 +0000

    New

---
+++ src/main/java/shapes/Rectangle.java
@@ -0,0 +1,22 @@
+package shapes;
+
+import shapes.Shape;
+
+import java.awt.*;
+public class Rectangle extends Shape {
+    private int w; // Fields
+    private int h;
+
+    public Rectangle(Point initPos, Color col, int width,int height){
+        super(initPos, col); // The constructor
+        w = width;
+        h  = height;
+    }
+
+    @Override
+    public void draw(Graphics g) { // A method that draws the object in g
+        g.setColor(col);
+        g.fillRect(pos.x,pos.y,w,h);
+
+    }
+}
